738ab8dda6d27b4959576862c0684c0069b62c64
haiflive <haiflive@mail.ru>
2019-05-16 12:33:06 +1000

    Add redis configuration;

---
+++ phpmyadmin/config.user.inc.php
@@ -0,0 +1,4 @@
+<?php
+$sessionValidity = 3600 * 24 * 365; // one year
+$cfg['LoginCookieValidity'] = $sessionValidity;
+ini_set('session.gc_maxlifetime', $sessionValidity);
---
+++ stat_memcached.bat
@@ -0,0 +1,1 @@
+docker-compose exec php memcached -d -m 512 -l 127.0.0.1 -p 11211 -u nobody
